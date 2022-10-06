8069df45a806652cb167bf15a58c5531e73ada7e
DaryBena <darinabezyakina@gmail.com>
2022-10-07 01:25:07 +0300

    Showing courent temperature

---
+++ src/index.js
@@ -0,0 +1,18 @@
+function displeyTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let speedElement = document.querySelector("#speed");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  speedElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let city = "New York";
+let apiKey = "7e5b57c35b922297644dec58a82f602c";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displeyTemperature);
